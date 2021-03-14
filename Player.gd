eef3a322cba5fc4598e959e6f4832bad5d1fb71e
Michael Lynch <git@mtlynch.io>
2021-03-13 21:22:07 -0500

    Adjusting heights and gravity

--- Player.gd
+++ Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var speed : int = 200
-var jumpForce : int = 500
+var jumpForce : int = 300
 var gravity : int = 200
 
 var vel : Vector2 = Vector2()
@@ -9,21 +9,21 @@ var vel : Vector2 = Vector2()
 onready var sprite : Sprite = get_node ("Sprite")
 
 func _physics_process(delta):
-	
+
 	vel.x = 0
-	
+
 	if Input.is_action_pressed("move_left"):
 		vel.x -= speed
-	if Input.is_action_pressed("move_right"): 
+	if Input.is_action_pressed("move_right"):
 		vel.x += speed
-		
+
 	vel = move_and_slide(vel, Vector2.UP)
-	
+
 	vel.y += gravity * delta
-	
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		vel.y -= jumpForce
-		
+
 	if vel.x<0:
 		sprite.flip_h = true
 	elif vel.x>0:
